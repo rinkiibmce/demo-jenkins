12c5a43bd4094ef397b92d6a7dc22f3af5ad316c
Rinki Gupta <rinki.gupta.13@gmail.com>
2025-10-30 22:32:05 +0530

    j to J

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello from sample-node-app with Jenkins!' });
+  res.json({ message: 'Hello from sample-node-app with Jenkins!' });
 });
 
 app.listen(PORT, () => {
