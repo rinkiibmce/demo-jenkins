4304da36d0ead9e018babba0c63e495f949d29e9
rinkiibmce <rinki.ibmce@gmail.com>
2025-10-30 22:12:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello from sample-node-app with Jenkins!' });
+ // res.send({ message: 'Hello from sample-node-app with Jenkins!' });
+ res.send('Hello from sample-node-app with Jenkins!');
+  
 });
 
 app.listen(PORT, () => {
